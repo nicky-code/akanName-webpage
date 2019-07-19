0cb352ddde4971c58e7623715fa982ae67f3a7ab
nicky-code <aline.nicole7@gmail.com>
2019-07-19 21:14:17 +0200

    added another condition when the user put invalid date.

--- js/scripts.js
+++ js/scripts.js
@@ -5,25 +5,31 @@
         var CC = (YY - 1) / 100 + 1;
         var dayoftheweek = (CC / 4 - 2 * CC - 1 + 5 * YY / 4 + (26 * (MM + 1) / 10) + DD) % 7;
         console.log(dayoftheweek);
-        // document.getElementById('result').innerHTML = Math.round(dayoftheweek);
+
 
         var men = ["Kwasi", "Kwaduo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
         var women = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
         var selection = document.getElementById('selector').value;
-
-        if (selection === "male") {
-            console.log(men[Math.round(dayoftheweek)]);
-
-            document.getElementById('ghanian').innerHTML = men[Math.round(dayoftheweek)];
-
-        } else if (selection === "female") {
-            console.log(women[Math.round(dayoftheweek)]);
-
-            document.getElementById('ghanian').innerHTML = women[Math.round(dayoftheweek)];
-
+        if (((DD < 0) || (DD >= 31)) && ((MM <= 1) || (MM >= 12))) {
+            alert("Please enter a valid date");
         } else {
-            console.log("invalid output");
+
+            if (selection === "male") {
+                console.log(men[Math.round(dayoftheweek)]);
+
+                document.getElementById('ghanian').innerHTML = men[Math.round(dayoftheweek)];
+            } else if (selection === "female") {
+                console.log(women[Math.round(dayoftheweek)]);
+
+                document.getElementById('ghanian').innerHTML = women[Math.round(dayoftheweek)];
+
+            } else {
+                console.log("invalid output");
+            }
+
         }
+
+
 
 
     }
